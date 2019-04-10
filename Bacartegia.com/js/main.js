b627410587869faf417ef9b4f7d2407b539199d8
AlbertoCastelan <albertovelacastelan@gmail.com>
2019-04-10 14:08:31 +0100

    Add files via upload

--- Bacartegia.com/js/main.js
+++ Bacartegia.com/js/main.js
@@ -4,26 +4,3 @@ $(document).ready(function() {
     $('nav').toggleClass('active');
   });
 });
-
-//  CREATE A SLIDESHOW
-var i = 0;
-var time = 2000;
-var images = [
-  'img/office-1.jpg',
-  'img/office-2.jpg',
-  'img/luxuryoffice.jpg',
-  'img/bookshelves.jpg',
-  'img/chairs.jpg',
-  'img/businesscoffee.jpg'
-];
-
-function changeImg() {
-  document.slide.src = images[i];
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
-}
-
-setInterval(changeImg, time);
--- Bacartegia.com/js/rooms.js
+++ Bacartegia.com/js/rooms.js
@@ -22,9 +22,58 @@ xhr.onload = function() {
 };
 
 function displayRoom(x) {
-  console.log(x);
+  //console.log(x);
   document.getElementById('office-name').innerHTML = officeInfo[x].name;
   document.getElementById('description').innerHTML = officeInfo[x].description;
   document.getElementById('photo').src = officeInfo[x].photo;
   document.getElementById('price').innerHTML = officeInfo[x].price;
+}
+
+var exec, deluxe, master, standard;
+exec = document.querySelector('.btn-1');
+deluxe = document.querySelector('.btn-2');
+master = document.querySelector('.btn-3');
+standard = document.querySelector('.btn-4');
+
+// exec.addEventListener('click', e => {
+//   e.preventDefault();
+//   displayRoom(0);
+// });
+
+// deluxe.addEventListener('click', e => {
+//   e.preventDefault();
+//   displayRoom(1);
+// });
+
+// master.addEventListener('click', e => {
+//   e.preventDefault();
+//   displayRoom(2);
+// });
+
+// standard.addEventListener('click', e => {
+//   e.preventDefault();
+//   displayRoom(3);
+// });
+
+var buttons = document.querySelectorAll('.btn');
+for (var i = 0; i < buttons.length; i++) {
+  var self = buttons[i];
+
+  self.addEventListener(
+    'click',
+    function(event) {
+      // prevent browser's default action
+      event.preventDefault();
+      // call your awesome function here
+      let values = document.getElementById(event.target.id);
+      if (values !== null) {
+        let numbers = Number(values.getAttribute('name'));
+        console.log(numbers);
+        displayRoom(numbers);
+      }
+
+      // 'this' refers to the current button on for loop
+    },
+    false
+  );
 }
